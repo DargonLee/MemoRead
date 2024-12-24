e145e79792acbcbbf3346ed2b3f81a3a83c68c83
Harlans <2461414445@qq.com>
2024-12-24 20:21:38 +0800

    init

---
+++ MemoRead/App/MemoReadApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoReadApp.swift
+//  MemoRead
+//
+//  Created by Harlans on 2024/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MemoReadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
